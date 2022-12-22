69d8050a270abbf4f29c2c4dd937337a91fa2bd3
Darina Ayurova <comedial@student.21-school.ru>
2022-12-22 15:18:23 +0300

    ceil floor fabs abs work

--- src/s21_fabs.c
+++ src/s21_fabs.c
@@ -1,7 +1,11 @@
 #include "s21_math.h"
 
 long double s21_fabs(double x) {
-  if (x == s21_INF || x == -s21_INF) return s21_INF;
-  if (x == s21_NAN || x == -s21_NAN) return s21_NAN;
-  return x >= 0 ? x : (-1) * x;
+  if (x == s21_INF || x == -s21_INF)
+    x = s21_INF;
+  if (x == s21_NAN || x == -s21_NAN)
+    x = s21_NAN;
+  else if (x < 0)
+    x = -1 * x;
+  return x;
 }
