335e5d9d16a07d922ef9e66fda1e68bd4e76f9bc
Darina Ayurova <comedial@student.21-school.ru>
2022-12-23 15:40:10 +0300

    100% без inf nan

--- src/s21_math.h
+++ src/s21_math.h
@@ -1,10 +1,15 @@
 #ifndef SRC_S21_MATH_H_
 #define SRC_S21_MATH_H_
 
-#define S21_PI 3.14159265358979323846
-
-#define s21_INF 1.00 / 0.00
-#define s21_NAN 0.00 / 0.00
+#define s21_PI 3.14159265358979323846
+#define s21_M_PI_2 1.57079632679489661923
+#define s21_M2_PI 0.636619772367581343076
+#define s21_e 2.7182818284590452353602874
+#define s21_INF 1.0 / 0.0
+#define s21_EPS 1e-9
+#define s21_DBL_MAX 1.7976931348623158e+308
+#define s21_NAN 0.0 / 0.0
+#define s21_M_LN10 2.30258509299404568402
 
 // вычисляет абсолютное значение целого числа
 int s21_abs(int x);
@@ -53,5 +58,8 @@ long double s21_tan(double x);
 
 // вычисляет факториал
 long double s21_fact(double x);
+
+// поиск Cos/Sin
+long double searchCosSin(double x, int n);
 
 #endif  // SRC_S21_MATH_H_
